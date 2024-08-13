9e08a5baaa8522ac7d2e78d8c98df4501229226c
Raghad <rsalshalan@gmail.com>
2024-08-13 09:54:35 +0000

    Initial commit

---
+++ app/frontend/src/components/Answer/AnswerIcon.tsx
@@ -0,0 +1,5 @@
+import { Sparkle28Filled } from "@fluentui/react-icons";
+
+export const AnswerIcon = () => {
+    return <Sparkle28Filled primaryFill={"rgba(255, 55, 94)"} aria-hidden="true" aria-label="Answer logo" />;
+};
---
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect, useRef, RefObject } from "react";
+import { Outlet, NavLink, Link } from "react-router-dom";
+import { IconButton } from "@fluentui/react";
+
+import styles from "./Layout.module.css";
+import { useLogin } from "../../authConfig";
+import { LoginButton } from "../../components/LoginButton";
+
+const Layout = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const menuRef: RefObject<HTMLDivElement> = useRef(null);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const handleClickOutside = (event: MouseEvent) => {
+        if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+            setMenuOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        if (menuOpen) {
+            document.addEventListener("mousedown", handleClickOutside);
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [menuOpen]);
+
+    return (
+        <div className={styles.layout}>
+            <header className={styles.header} role={"banner"}>
+                <div className={styles.headerContainer} ref={menuRef}>
+                    <Link to="/" className={styles.headerTitleContainer}>
+                        <h3 className={styles.headerTitle}>STC brain : Sales Enablement Chatbot</h3>
+                    </Link>
+                    <nav>
+                        <ul className={`${styles.headerNavList} ${menuOpen ? styles.show : ""}`}>
+                            <li>
+                                <NavLink
+                                    to="/"
+                                    className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}
+                                    onClick={() => setMenuOpen(false)}
+                                >
+                                    Chat
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/qa"
+                                    className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}
+                                    onClick={() => setMenuOpen(false)}
+                                >
+                                    Ask a question
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className={styles.loginMenuContainer}>
+                        {useLogin && <LoginButton />}
+                        <IconButton iconProps={{ iconName: "GlobalNavButton" }} className={styles.menuToggle} onClick={toggleMenu} ariaLabel="Toggle menu" />
+                    </div>
+                </div>
+            </header>
+
+            <main className={styles.mainContent}>
+                <Outlet />
+            </main>
+
+            <footer className={styles.footer}>
+                <p>This chatbot provides information based on available data and does not guarantee accuracy or completeness. copyright stc 2024</p>
+            </footer>
+        </div>
+    );
+};
+
+export default Layout;
